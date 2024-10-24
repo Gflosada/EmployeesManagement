66a78d30e7f4fd54f85cfe7e2272ad40b8939ac8
German <gflosadaz@gmail.com>
2024-10-24 14:02:37 -0400

    done

---
+++ Context/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using EmployeesManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeesManagement.Context
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> contextOptions)
+            : base(contextOptions)
+        {
+            
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployeesManagement.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string MiddleName { get; set; }
+        public string lastName { get; set; }
+        public string EmailAddress { get; set; }
+        public int PhoneNo { get; set; }
+
+    }
+}
